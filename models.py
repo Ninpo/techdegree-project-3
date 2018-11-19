71bd7ef18f86dbe6b34d14b5610046349b19ff5a
Alex Boag-Munroe <ninpo@qap.la>
2018-11-19 17:55:53 +0000

    Consolidated models using Marshmallow for validation and sane serialisation

--- models.py
+++ models.py
@@ -1,13 +1,15 @@
 import json
+from marshmallow import Schema, fields, post_dump, post_load
 
 
-class JSONModel:
-    def __init__(self, json_file="scratch.json"):
+class JSONStore:
+    def __init__(self, json_file="tasks.json"):
         self.json_file = json_file
+
         try:
             with open(self.json_file, 'r') as data_file:
                 self.data = json.load(data_file)
-        except (FileNotFoundError, json.decoder.JSONDecodeError):
+        except FileNotFoundError:
             self.data = []
 
     def add(self, record):
@@ -16,21 +18,30 @@ class JSONModel:
             json.dump(self.data, data_file)
 
 
-class Task(JSONModel):
-    def __init__(self, **kwargs):
-        for k, v in kwargs.items():
-            setattr(self, k, v)
+class Task:
+    def __init__(self, date, title, time_spent, notes):
+        self.date = date
+        self.title = title
+        self.time_spent = time_spent
+        self.notes = notes
+
+    def __repr__(self):
+        return '<Task(title={self.title!r})>'.format(self=self)
+
+
+class TaskSchema(Schema):
+    def __init__(self):
+        self.json_store = JSONStore()
         super().__init__()
+    date = fields.DateTime(format="%d/%m/%Y", required=True)
+    title = fields.Str(required=True)
+    time_spent = fields.Int(required=True)
+    notes = fields.Str()
 
-    def edit(self):
-        pass
+    @post_load
+    def make_task(self, data):
+        return Task(**data)
 
-    def delete(self):
-        pass
-
-    def date_search(self):
-        pass
-
-    def text_search(self):
-        pass
-
+    @post_dump
+    def save_task(self, data):
+        self.json_store.add(data)
