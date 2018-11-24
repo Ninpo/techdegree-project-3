2651ebaf9c612c41cbdedb40329c6d98ac2460b8
Alex Boag-Munroe <ninpo@qap.la>
2018-11-24 03:19:37 +0000

    Abstract data manipulation away from Controller

---
+++ repositories.py
@@ -0,0 +1,26 @@
+from models import JSONStore, TaskSchema
+
+
+class DataRepo:
+    def __init__(self, json_file):
+        self.json_source = JSONStore(json_file)
+        self.data_schema = TaskSchema()
+
+    def get_records(self):
+        return self.data_schema.load(self.json_source.data, many=True)
+
+    def validate_fields(self, fields):
+        return self.data_schema.load(fields, partial=True)
+
+    def add_record(self, data):
+        record_obj = self.data_schema.load(data)
+        self.json_source.data.append(self.data_schema.dump(record_obj))
+        self.json_source.save()
+        return record_obj
+
+    def save_changes(self, updated_collection):
+        self.json_source.data = self.data_schema.dump(updated_collection, many=True)
+        self.json_source.save()
+
+    def save_data(self):
+        pass
