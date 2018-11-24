67a2c52049dfebf757b4354e896c5072c7a04153
Alex Boag-Munroe <ninpo@qap.la>
2018-11-24 03:20:26 +0000

    App initialisation logic updates

--- work_log.py
+++ work_log.py
@@ -1,9 +1,24 @@
-from views import MainView
+"""CLI Task Logger
+
+Author: Alex Boag-Munroe"""
+
+from json.decoder import JSONDecodeError
+from controllers import TaskController
+from repositories import DataRepo
 
 
 def main():
-    view = MainView()
-    view.present_view()
+    """App initialisation.
+    """
+    json_file = "tasks.json"
+    try:
+        data_interface = DataRepo(json_file)
+    except JSONDecodeError as err:
+        print("Invalid JSON file {} detected.".format(json_file))
+        print("JSON error: {}".format(err))
+        return
+    task_app = TaskController(data_interface)
+    task_app.start()
 
 
 if __name__ == "__main__":
